6e6a859cde0276ea686ac26836f94ff390d7bd2e
Tommy Tran <haitran1995@gmail.com>
2022-09-05 13:05:17 -0400

    added movement and quick message hotkeys

--- client.js
+++ client.js
@@ -1,17 +1,19 @@
 const net = require("net");
+const { IP, PORT } = require("./constants");
 const { rawListeners } = require("process");
-
 const connect = function () { // <== establishes a connection with the game server
   const conn = net.createConnection({
-    host: "localhost",
-    port: 50541,
+    host: IP,
+    port: PORT,
   });
+
 
   conn.setEncoding("utf8"); // <== Turn incoming data as text.
   conn.on("data", (data) => console.log(data))
   conn.on("connect", (connect) => console.log("Successfully connected to game server"))
   conn.write("Name: TTT ")
-  conn.write("Move: Up")
+  
+  conn.write("Say: Hello")
   
   return conn;
 };
--- input.js
+++ input.js
@@ -1,16 +1,37 @@
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.on("data", handleUserInput);
-  return stdin;
-};
+let connection;
 
-const handleUserInput = (key) => {
+
+const handleUserInput = (key, conn) => {
   if (key === '\u0003') {
     process.exit();
   };
+  if (key === 'w') {
+    conn.write("Move: up")
+  }
+  if (key === 'a') {
+    conn.write("Move: left")
+  }
+  if (key === 's') {
+    conn.write("Move: down")
+  }
+  if (key === 'd') {
+    conn.write("Move: right")
+  }
+  if (key === 'h') {
+    conn.write("Say: Hello Byran")
+  }
 }
+
+const setupInput = function (conn) {
+  connection = conn;
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume(); 
+  stdin.on("data", (data) => {
+    handleUserInput(data, conn)});
+  
+  return stdin;
+};
 
 module.exports = setupInput;
